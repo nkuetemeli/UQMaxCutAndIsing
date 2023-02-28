5f255a23f7e40bb4929974199a3aabde4965e2dd
kuetemeli <n.kuetemeli@uni-luebeck.de>
2023-02-28 10:26:44 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='UQMaxCutAndIsing',
+      version='1.0',
+      description='Universal Quantum Algorithms for MaxCut and Ising',
+      author='Natacha Kuete Meli',
+      author_email='meli@mic.uni-luebeck.de',
+      url='https://#',
+      packages=['../src'],
+     )
